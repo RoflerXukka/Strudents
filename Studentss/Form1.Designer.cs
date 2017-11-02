26a0b23df534ddeb608e0387c1bf5db971e327e0
Alex <sashafriend2000@mail.ru>
2017-11-02 18:32:24 +0300

    Хрень

---
+++ Studentss/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Studentss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.DataGridView();
+            this.Namee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SecondName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mark = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SelectionMode = new System.Windows.Forms.ComboBox();
+            this.View = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ResetView = new System.Windows.Forms.Button();
+            this.secondtable = new System.Windows.Forms.DataGridView();
+            this.Subject = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MarkOfS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondtable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.AllowUserToAddRows = false;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Namee,
+            this.SecondName,
+            this.Group,
+            this.Mark});
+            this.table.Location = new System.Drawing.Point(12, 12);
+            this.table.Name = "table";
+            this.table.ReadOnly = true;
+            this.table.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.table.Size = new System.Drawing.Size(461, 206);
+            this.table.TabIndex = 0;
+            this.table.SelectionChanged += new System.EventHandler(this.table_SelectionChanged);
+            // 
+            // Namee
+            // 
+            this.Namee.HeaderText = "Имя";
+            this.Namee.Name = "Namee";
+            this.Namee.ReadOnly = true;
+            // 
+            // SecondName
+            // 
+            this.SecondName.HeaderText = "Фамилия";
+            this.SecondName.Name = "SecondName";
+            this.SecondName.ReadOnly = true;
+            // 
+            // Group
+            // 
+            this.Group.HeaderText = "Группа";
+            this.Group.Name = "Group";
+            this.Group.ReadOnly = true;
+            // 
+            // Mark
+            // 
+            this.Mark.HeaderText = "Оценка";
+            this.Mark.Name = "Mark";
+            this.Mark.ReadOnly = true;
+            // 
+            // SelectionMode
+            // 
+            this.SelectionMode.FormattingEnabled = true;
+            this.SelectionMode.Items.AddRange(new object[] {
+            "По имени",
+            "По группе",
+            "Неудачники"});
+            this.SelectionMode.Location = new System.Drawing.Point(51, 248);
+            this.SelectionMode.Name = "SelectionMode";
+            this.SelectionMode.Size = new System.Drawing.Size(121, 21);
+            this.SelectionMode.TabIndex = 1;
+            this.SelectionMode.SelectedIndexChanged += new System.EventHandler(this.SelectionMode_SelectedIndexChanged);
+            // 
+            // View
+            // 
+            this.View.Location = new System.Drawing.Point(350, 245);
+            this.View.Name = "View";
+            this.View.Size = new System.Drawing.Size(75, 23);
+            this.View.TabIndex = 2;
+            this.View.Text = "Найти";
+            this.View.UseVisualStyleBackColor = true;
+            this.View.Click += new System.EventHandler(this.View_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(194, 248);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // ResetView
+            // 
+            this.ResetView.Location = new System.Drawing.Point(431, 245);
+            this.ResetView.Name = "ResetView";
+            this.ResetView.Size = new System.Drawing.Size(112, 23);
+            this.ResetView.TabIndex = 4;
+            this.ResetView.Text = "Сброс поиска";
+            this.ResetView.UseVisualStyleBackColor = true;
+            this.ResetView.Click += new System.EventHandler(this.ResetView_Click);
+            // 
+            // secondtable
+            // 
+            this.secondtable.AllowUserToAddRows = false;
+            this.secondtable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.secondtable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Subject,
+            this.MarkOfS});
+            this.secondtable.Location = new System.Drawing.Point(517, 12);
+            this.secondtable.Name = "secondtable";
+            this.secondtable.ReadOnly = true;
+            this.secondtable.Size = new System.Drawing.Size(286, 150);
+            this.secondtable.TabIndex = 5;
+            // 
+            // Subject
+            // 
+            this.Subject.HeaderText = "Предмет";
+            this.Subject.Name = "Subject";
+            this.Subject.ReadOnly = true;
+            this.Subject.Width = 140;
+            // 
+            // MarkOfS
+            // 
+            this.MarkOfS.HeaderText = "Оценка";
+            this.MarkOfS.Name = "MarkOfS";
+            this.MarkOfS.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 297);
+            this.Controls.Add(this.secondtable);
+            this.Controls.Add(this.ResetView);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.View);
+            this.Controls.Add(this.SelectionMode);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondtable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Namee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SecondName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Group;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mark;
+        private System.Windows.Forms.ComboBox SelectionMode;
+        private System.Windows.Forms.Button View;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ResetView;
+        private System.Windows.Forms.DataGridView secondtable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subject;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MarkOfS;
+    }
+}
+
---
+++ Studentss/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Studentss
+{
+    public partial class Form1 : Form
+    {
+        string[] predmeti = { "Математика", "ТРПО", "КПИЯП", "Моделирование", "Русский язык" };
+        bool isLoaded = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List< Dictionary<string, double> > SubjectsAndMarks;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SubjectsAndMarks = new List<Dictionary<string, double>>();
+            DataSet data = new DataSet();
+            data.ReadXml("BD.xml");
+            //int index = 0;
+            foreach (DataRow row in data.Tables["Students"].Rows)
+            {
+                int n = table.Rows.Add();
+                table.Rows[n].Cells[0].Value = row["Name"];
+                table.Rows[n].Cells[1].Value = row["SecondName"];
+                table.Rows[n].Cells[2].Value = row["Group"];
+                string[] sandm = row["Subject"].ToString().Split(';');
+                double midlemark = 0;
+                Dictionary<string, double> subandmar = new Dictionary<string, double>();
+                foreach (string str in sandm)
+                {
+                    string[] datas = str.Split('!');
+                    subandmar[datas[0]] = double.Parse(datas[1]);
+                    midlemark += double.Parse(datas[1]);
+                }
+                SubjectsAndMarks.Add(subandmar);
+                midlemark /= SubjectsAndMarks[n].Count;
+                table.Rows[n].Cells[3].Value = midlemark;
+            }
+            isLoaded = true;
+        }
+
+        private void SelectionMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SelectionMode.SelectedIndex == 2)
+            {
+                View.Text = "Показать";
+                textBox1.Visible = false;
+            }
+            else
+            {
+                View.Text = "Найти";
+                textBox1.Visible = true;
+            }
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+            switch (SelectionMode.SelectedIndex)
+            {
+                case 0:
+                    for(int i = 0; i < table.RowCount; i++)
+                    {
+                        if(table.Rows[i].Cells[0].Value.ToString().ToUpper() != textBox1.Text.ToUpper())
+                        {
+                            table.Rows[i].Visible = false;
+                        }
+                    }
+                    break;
+                case 1:
+                    for (int i = 0; i < table.RowCount; i++)
+                    {
+                        if (table.Rows[i].Cells[2].Value.ToString().ToUpper() != textBox1.Text.ToUpper())
+                        {
+                            table.Rows[i].Visible = false;
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < table.RowCount; i++)
+                    {
+                        if (double.Parse(table.Rows[i].Cells[3].Value.ToString()) > 4f)
+                        {
+                            table.Rows[i].Visible = false;
+                        }
+                    }
+                    break;
+                default:
+                    MessageBox.Show("Выберите режим поиска/отображения");
+                    return;
+            }
+        }
+
+        private void ResetView_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < table.RowCount; i++)
+                table.Rows[i].Visible = true;
+        }
+
+        private void table_SelectionChanged(object sender, EventArgs e)
+        {
+            secondtable.Rows.Clear();
+            if (!this.isLoaded) return;
+            for(int i = 0; i < SubjectsAndMarks[table.CurrentRow.Index].Count; i++)
+            {
+                int n = secondtable.Rows.Add();
+                secondtable.Rows[n].Cells[0].Value = predmeti[i];
+                secondtable.Rows[n].Cells[1].Value = SubjectsAndMarks[table.CurrentRow.Index][predmeti[i]];
+            }
+        }
+    }
+}
